e04997ce3c2a8078627122f9c8037ffdaa65a92e
AdamSkubida <adasiow.s@gmail.com>
2023-04-22 15:00:28 +0200

    future/galleryItems

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const addImagesToGallery = galleryItems.reduce((acc, newImage) => {
+  return (
+    acc +
+    `<li class="gallery__item"><img src="${newImage.preview}" alt="${newImage.description}" class="gallery__image gallery__link"></li>`
+  );
+}, "");
+
+galleryList.insertAdjacentHTML("beforeend", addImagesToGallery);
